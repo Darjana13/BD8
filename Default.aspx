09f2c227fde967545dc36f98d5565ec1c19a9c06
leonovich <leonovich.d.a13@gmail.com>
2021-12-04 00:31:38 +0700

    Добавьте файлы проекта.

---
+++ Default.aspx
@@ -0,0 +1,69 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="WebApplication1.Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+
+            <asp:SqlDataSource ID="SqlDataSource1" runat="server" ConnectionString="<%$ ConnectionStrings:studentsConnectionString4 %>" ProviderName="<%$ ConnectionStrings:studentsConnectionString4.ProviderName %>" SelectCommand="SELECT name, n_izd FROM pmib8307.j"></asp:SqlDataSource>
+
+            <asp:SqlDataSource ID="SqlDataSource2" runat="server" ConnectionString="<%$ ConnectionStrings:studentsConnectionString4 %>" ProviderName="<%$ ConnectionStrings:studentsConnectionString4.ProviderName %>" SelectCommand="Select * from pmib8307.p"></asp:SqlDataSource>
+
+            <asp:SqlDataSource ID="SqlDataSource3" runat="server"></asp:SqlDataSource>
+
+        </div>
+        <table width="100%" cellspacing="0" cellpadding="5">
+        <tr> 
+            <td width="50%" valign="top">
+                 Задание 1.
+                Получить информацию о рекомендованной цене на указанное изделие на заданную дату.
+         <p>
+             Выберете изделие:
+            <asp:DropDownList ID="DropDownList1" runat="server">
+            </asp:DropDownList>
+         </p>
+                 <p>
+             Выберете дату:<asp:Calendar ID="Calendar1" runat="server"></asp:Calendar>
+        <br />
+        <asp:Button ID="Button1" runat="server" OnClick="Button1_Click" Text="Рекомендованная цена" />
+        <br />
+        <br />
+        <asp:Label ID="Label1" runat="server" Text="Label"></asp:Label>
+         </p>
+            </td>
+            <td valign="top">
+                Задание 2. Для изделий, в состав которых входит заданная деталь, сдвинуть на месяц назад дату начала действия последней рекомендованной цены.
+        <p>
+            Выберете деталь:
+            <asp:DropDownList ID="DropDownList3" runat="server" OnSelectedIndexChanged="DropDownList3_SelectedIndexChanged">
+            </asp:DropDownList>
+        </p>
+                <p>
+            <asp:Button ID="Button2" runat="server" OnClick="DropDownList3_SelectedIndexChanged" Text="Показать текущие данные" />
+        </p>
+                <p>
+            <asp:GridView ID="GridView1" runat="server">
+                 
+            </asp:GridView>
+        </p>
+                <p>
+                    <asp:Button ID="Button3" runat="server" OnClick="Button3_Click" Text="Изменить даты" />
+        </p>
+                <p>
+                    <asp:Label ID="Label2" runat="server" Text="Label"></asp:Label>
+        </p>
+            </td>
+        </tr>
+        </table>
+        
+        <asp:Label ID="Label3" runat="server" Text="Label"></asp:Label>
+        
+    </form>
+</body>
+</html>
---
+++ Default.aspx.designer.cs
@@ -0,0 +1,141 @@
+﻿//------------------------------------------------------------------------------
+// <автоматически создаваемое>
+//     Этот код создан программой.
+//
+//     Изменения в этом файле могут привести к неправильной работе и будут потеряны в случае
+//     повторной генерации кода. 
+// </автоматически создаваемое>
+//------------------------------------------------------------------------------
+
+namespace WebApplication1
+{
+   public partial class Default
+   {
+
+      /// <summary>
+      /// form1 элемент управления.
+      /// </summary>
+      /// <remarks>
+      /// Автоматически создаваемое поле.
+      /// Для изменения переместите объявление поля из файла конструктора в файл кода программной части.
+      /// </remarks>
+      protected global::System.Web.UI.HtmlControls.HtmlForm form1;
+
+      /// <summary>
+      /// SqlDataSource1 элемент управления.
+      /// </summary>
+      /// <remarks>
+      /// Автоматически создаваемое поле.
+      /// Для изменения переместите объявление поля из файла конструктора в файл кода программной части.
+      /// </remarks>
+      protected global::System.Web.UI.WebControls.SqlDataSource SqlDataSource1;
+
+      /// <summary>
+      /// SqlDataSource2 элемент управления.
+      /// </summary>
+      /// <remarks>
+      /// Автоматически создаваемое поле.
+      /// Для изменения переместите объявление поля из файла конструктора в файл кода программной части.
+      /// </remarks>
+      protected global::System.Web.UI.WebControls.SqlDataSource SqlDataSource2;
+
+      /// <summary>
+      /// SqlDataSource3 элемент управления.
+      /// </summary>
+      /// <remarks>
+      /// Автоматически создаваемое поле.
+      /// Для изменения переместите объявление поля из файла конструктора в файл кода программной части.
+      /// </remarks>
+      protected global::System.Web.UI.WebControls.SqlDataSource SqlDataSource3;
+
+      /// <summary>
+      /// DropDownList1 элемент управления.
+      /// </summary>
+      /// <remarks>
+      /// Автоматически создаваемое поле.
+      /// Для изменения переместите объявление поля из файла конструктора в файл кода программной части.
+      /// </remarks>
+      protected global::System.Web.UI.WebControls.DropDownList DropDownList1;
+
+      /// <summary>
+      /// Calendar1 элемент управления.
+      /// </summary>
+      /// <remarks>
+      /// Автоматически создаваемое поле.
+      /// Для изменения переместите объявление поля из файла конструктора в файл кода программной части.
+      /// </remarks>
+      protected global::System.Web.UI.WebControls.Calendar Calendar1;
+
+      /// <summary>
+      /// Button1 элемент управления.
+      /// </summary>
+      /// <remarks>
+      /// Автоматически создаваемое поле.
+      /// Для изменения переместите объявление поля из файла конструктора в файл кода программной части.
+      /// </remarks>
+      protected global::System.Web.UI.WebControls.Button Button1;
+
+      /// <summary>
+      /// Label1 элемент управления.
+      /// </summary>
+      /// <remarks>
+      /// Автоматически создаваемое поле.
+      /// Для изменения переместите объявление поля из файла конструктора в файл кода программной части.
+      /// </remarks>
+      protected global::System.Web.UI.WebControls.Label Label1;
+
+      /// <summary>
+      /// DropDownList3 элемент управления.
+      /// </summary>
+      /// <remarks>
+      /// Автоматически создаваемое поле.
+      /// Для изменения переместите объявление поля из файла конструктора в файл кода программной части.
+      /// </remarks>
+      protected global::System.Web.UI.WebControls.DropDownList DropDownList3;
+
+      /// <summary>
+      /// Button2 элемент управления.
+      /// </summary>
+      /// <remarks>
+      /// Автоматически создаваемое поле.
+      /// Для изменения переместите объявление поля из файла конструктора в файл кода программной части.
+      /// </remarks>
+      protected global::System.Web.UI.WebControls.Button Button2;
+
+      /// <summary>
+      /// GridView1 элемент управления.
+      /// </summary>
+      /// <remarks>
+      /// Автоматически создаваемое поле.
+      /// Для изменения переместите объявление поля из файла конструктора в файл кода программной части.
+      /// </remarks>
+      protected global::System.Web.UI.WebControls.GridView GridView1;
+
+      /// <summary>
+      /// Button3 элемент управления.
+      /// </summary>
+      /// <remarks>
+      /// Автоматически создаваемое поле.
+      /// Для изменения переместите объявление поля из файла конструктора в файл кода программной части.
+      /// </remarks>
+      protected global::System.Web.UI.WebControls.Button Button3;
+
+      /// <summary>
+      /// Label2 элемент управления.
+      /// </summary>
+      /// <remarks>
+      /// Автоматически создаваемое поле.
+      /// Для изменения переместите объявление поля из файла конструктора в файл кода программной части.
+      /// </remarks>
+      protected global::System.Web.UI.WebControls.Label Label2;
+
+      /// <summary>
+      /// Label3 элемент управления.
+      /// </summary>
+      /// <remarks>
+      /// Автоматически создаваемое поле.
+      /// Для изменения переместите объявление поля из файла конструктора в файл кода программной части.
+      /// </remarks>
+      protected global::System.Web.UI.WebControls.Label Label3;
+   }
+}
